a63f611101da43bce07eddc12f8a6030eb05b75a
Sep <esparago.joseph0002@gmail.com>
2023-01-27 23:32:50 +0800

    Add BigNFTSlider.jsx

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 //Internal Import
 import Style from '../styles/index.module.css';
-import { HeroSection, Service } from '../components/componentsindex'
+import { HeroSection, Service, BigNFTSlider } from '../components/componentsindex'
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection/>
       <Service/>
+      <BigNFTSlider/>
+
     </div>
   )
 }
