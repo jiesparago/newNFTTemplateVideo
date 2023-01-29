19bfd549fc0ad0cf47c1deab21d074f05b98da96
Sep <esparago.joseph0002@gmail.com>
2023-01-30 01:12:26 +0800

    Added filter.jsx

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 //Internal Import
 import Style from '../styles/index.module.css';
-import { HeroSection, Service, BigNFTSlider, Subscribe, Title, Category } from '../components/componentsindex'
+import { HeroSection, Service, BigNFTSlider, Subscribe, Title, Category, Filter } from '../components/componentsindex'
 
 const Home = () => {
   return (
@@ -10,6 +10,8 @@ const Home = () => {
       <HeroSection/>
       <Service/>
       <BigNFTSlider/>
+      <Title heading="Featured NFTs" paragraph="Discover the most outstanding NFTs in all topics of life."/>
+      <Filter/>
       <Title heading="Browse by Category" paragraph="Explore the NFTs in the most feature categories"/>
       <Category/>
       <Subscribe/>
